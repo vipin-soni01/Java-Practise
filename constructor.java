8440e04c17279a89f4ad41a408131ed4182be0cd
vipin-soni01 <sonivipin553@gmail.com>
2026-02-20 00:14:26 +0530

    GameLevel.class Enumeration of the GameLevel class, which represents different levels in a game. Each level has a name and a difficulty rating.

--- constructor.java
+++ constructor.java
@@ -30,68 +30,98 @@
 //     }
 // }
 
+// import java.util.*;
+// public class constructor{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         String model;
+//         System.out.print("Enter the model of your PC / Laptop: ");
+//         model = sc.nextLine();
+
+//         String name;
+//         System.out.print("Enter the name of your PC / Laptop: ");
+//         name = sc.nextLine();
+
+//         int year;
+//         System.out.print("Enter the manufacturing year of your PC / Laptop: ");
+//         year = sc.nextInt();
+
+//         int price;
+//         System.out.print("Enter the price of your PC / Laptop: ");
+//         price = sc.nextInt();
+
+
+
+//         Computer com = new Computer(model , name , year , price);
+
+//         com.details();
+//         com.buy();
+//     }
+// }
+// class Computer{
+//     String name;
+//     String model;
+//     int year;
+//     int price;
+//     Scanner sc = new Scanner(System.in);
+//     Computer(String name , String model , int year ,int price){
+//         this.model = model;
+//         this.name = name;
+//         this.year = year;
+//         this.price = price;
+//     }
+
+//     void details(){
+//         System.out.println("Your PC / Laptops details are: ");
+
+//         System.out.println("Name : "+this.name);
+//         System.out.println("Model : "+this.model);
+//         System.out.println("Manufacturing Year: "+this.year);
+//         System.out.println("Price :"+this.price);
+
+//     }
+
+//     void buy(){
+//         String purchase;
+//         System.out.print("Do you want to buy this ?(yes / no) : ");
+//         purchase = sc.nextLine();
+
+//         if(purchase.equals("yes")){
+//             System.out.println("Thank you , Come Again !!");
+//         }
+//         else{
+//             System.out.println("Visit Again and buy next time !");
+//         }
+//     }
+// }
+
+
+
+
+// Enum
 import java.util.*;
+enum GameLevel{
+    EASY , MEDIUM , HARD
+}
 public class constructor{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String model;
-        System.out.print("Enter the model of your PC / Laptop: ");
-        model = sc.nextLine();
+        System.out.print("Enter your Score: ");
+        int score = sc.nextInt();
+        GameLevel level;
 
-        String name;
-        System.out.print("Enter the name of your PC / Laptop: ");
-        name = sc.nextLine();
-
-        int year;
-        System.out.print("Enter the manufacturing year of your PC / Laptop: ");
-        year = sc.nextInt();
-
-        int price;
-        System.out.print("Enter the price of your PC / Laptop: ");
-        price = sc.nextInt();
-
-
-
-        Computer com = new Computer(model , name , year , price);
-
-        com.details();
-        com.buy();
-    }
-}
-class Computer{
-    String name;
-    String model;
-    int year;
-    int price;
-    Scanner sc = new Scanner(System.in);
-    Computer(String name , String model , int year ,int price){
-        this.model = model;
-        this.name = name;
-        this.year = year;
-        this.price = price;
-    }
-
-    void details(){
-        System.out.println("Your PC / Laptops details are: ");
-
-        System.out.println("Name : "+this.name);
-        System.out.println("Model : "+this.model);
-        System.out.println("Manufacturing Year: "+this.year);
-        System.out.println("Price :"+this.price);
-
-    }
-
-    void buy(){
-        String purchase;
-        System.out.print("Do you want to buy this ?(yes / no) : ");
-        purchase = sc.nextLine();
-
-        if(purchase.equals("yes")){
-            System.out.println("Thank you , Come Again !!");
+        if(score >0 && score <= 50){
+            level = GameLevel.EASY;
+        }
+        else if(score > 50 &&  score <= 75){
+            level = GameLevel.MEDIUM;
         }
         else{
-            System.out.println("Visit Again and buy next time !");
+            level = GameLevel.HARD;
         }
+        System.out.print(level);
     }
 }
+
