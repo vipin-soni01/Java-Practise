d066a1fdb11709ecb295b5d5c51e3d892c275a46
vipin-soni01 <sonivipin553@gmail.com>
2026-03-01 23:53:03 +0530

    read this

--- practise.java
+++ practise.java
@@ -2,6 +2,9 @@ public class practise {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+    public static void add(int a, int b) {
+        System.out.println(a + b);
+    }
     
     
 }
